8f54e9eb38837d02c1f7d8043d3f099c127dab8f
droidcore <agusmahardika99@gmail.com>
2025-11-11 11:48:13 +0700

    av

--- superior_peridot.mk
+++ superior_peridot.mk
@@ -9,24 +9,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common_full_phone.mk)
 
 # Inherit from peridot device
 $(call inherit-product, device/xiaomi/peridot/device.mk)
 
 # Superior
 WITH_GMS := true
-TARGET_USES_PICO_GAPPS := true
 TARGET_OPTIMIZED_DEXOPT := true
 TARGET_ENABLE_BLUR := true
 TARGET_HAS_UDFPS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-RISING_MAINTAINER= BLU
 
 # Device Flags
-PRODUCT_NAME := lineage_peridot
+PRODUCT_NAME := superior_peridot
 PRODUCT_DEVICE := peridot
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
