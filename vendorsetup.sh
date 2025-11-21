e04904f42b12128da88e516443499ed636afe5df
zenin1504 <kayozzprojects@yandex.com>
2025-11-21 08:09:27 +0000

    peridot: Pin all firmware
    * Saves time for us instead dumping and unpacking payload again and again (probably gonna freeze on this OS2.0.206.0VNPMIXM version)
    
    Change-Id: If3497211c484468b8689955cda920fe3
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,22 +3,47 @@
 # Vendor (fresh clone)
 echo "Cloning vendor tree..."
 rm -rf vendor/xiaomi/peridot
+<<<<<<< HEAD
 git clone -b los https://github.com/droidcore/f6_proprietary_vendor_xiaomi_peridot.git vendor/xiaomi/peridot
+=======
+git clone -b lineage-23.1 https://github.com/sm8635-dev/vendor_xiaomi_peridot.git vendor/xiaomi/peridot
+>>>>>>> c4ab372 (peridot: Pin all firmware)
 
 # Kernel source (fresh clone)
 echo "Cloning kernel source tree..."
 rm -rf kernel/xiaomi/sm8635
+<<<<<<< HEAD
 git clone -b lineage-23.0 --depth 1 https://github.com/droidcore/android_kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635
 rm -rf kernel/xiaomi/sm8635-modules
 git clone -b lineage-23.0 --depth 1 https://github.com/droidcore/android_kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
 
 rm -rf kernel/xiaomi/sm8635-devicetrees
 git clone -b lineage-23.0 --depth 1 https://github.com/peridot-dev/android_kernel_xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
+=======
+git clone -b lineage-23.1 --depth 1 https://github.com/sm8635-dev/kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635
+
+rm -rf kernel/xiaomi/sm8635-modules
+git clone -b lineage-23.1 https://github.com/sm8635-dev/kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
+
+rm -rf kernel/xiaomi/sm8635-devicetrees
+git clone -b lineage-23.1 https://github.com/sm8635-dev/kernel_xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
+
+# MiuiCamera device tree (fresh clone)
+echo "Cloning MiuiCamera device tree..."
+rm -rf device/xiaomi/peridot-miuicamera
+git clone https://github.com/sm8635-dev/device_xiaomi_peridot-miuicamera.git device/xiaomi/peridot-miuicamera
+
+# MiuiCamera vendor tree (fresh clone)
+echo "Cloning MiuiCamera vendor tree..."
+rm -rf vendor/xiaomi/peridot-miuicamera
+git clone https://github.com/sm8635-dev/vendor_xiaomi_peridot-miuicamera.git vendor/xiaomi/peridot-miuicamera
+>>>>>>> c4ab372 (peridot: Pin all firmware)
 
 # Hardware xiaomi (fresh clone)
 echo "Cloning hardware xiaomi source..."
 rm -rf hardware/xiaomi
 git clone -b lineage-23.1 https://github.com/sm8635-dev/hardware_xiaomi.git hardware/xiaomi
+<<<<<<< HEAD
 
 # MiuiCamera device tree (fresh clone)
 echo "Cloning MiuiCamera device tree..."
@@ -34,16 +59,25 @@ git clone https://github.com/F6-test/vendor-xiaomi-peridot-miuicamera.git vendor
 echo "Cloning Viper4Android tree..."
 rm -rf packages/apps/ViPER4AndroidFX
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
+=======
+>>>>>>> c4ab372 (peridot: Pin all firmware)
 
 # KProfiles (fresh clone)
 echo "Cloning KProfiles..."
 rm -rf packages/apps/KProfiles
 git clone -b lineage-23.1 https://github.com/sm8635-dev/packages_apps_KProfiles.git packages/apps/KProfiles
 
+<<<<<<< HEAD
 # Gamebar
 echo "Cloning Gamebar tree..."
 rm -rf packages/apps/GameBar
 git clone https://github.com/droidcore/packages_apps_GameBar.git packages/apps/GameBar
+=======
+# FastCharge (fresh clone)
+echo "Cloning FastCharge..."
+rm -rf packages/apps/FastCharge
+git clone -b lineage-23.1 https://github.com/sm8635-dev/packages_apps_FastCharge.git packages/apps/FastCharge
+>>>>>>> c4ab372 (peridot: Pin all firmware)
 
 # Compat (cherry pick)
 echo "Fetching QPR1 compat..."
