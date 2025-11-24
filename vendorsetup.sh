6e699e0495e17f3cd345de2816118cf12f5cd670
droidcore <agusmahardika99@gmail.com>
2025-11-24 18:13:00 +0700

    vendor

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,47 +3,26 @@
 # Vendor (fresh clone)
 echo "Cloning vendor tree..."
 rm -rf vendor/xiaomi/peridot
-<<<<<<< HEAD
-git clone -b los https://github.com/droidcore/f6_proprietary_vendor_xiaomi_peridot.git vendor/xiaomi/peridot
-=======
-git clone -b lineage-23.1 https://github.com/sm8635-dev/vendor_xiaomi_peridot.git vendor/xiaomi/peridot
->>>>>>> c4ab372 (peridot: Pin all firmware)
+git clone -b lineage-23.1 https://gitea.com/zenin/vendor_xiaomi_peridot.git vendor/xiaomi/peridot
 
 # Kernel source (fresh clone)
 echo "Cloning kernel source tree..."
 rm -rf kernel/xiaomi/sm8635
-<<<<<<< HEAD
-git clone -b lineage-23.0 --depth 1 https://github.com/droidcore/android_kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635
+git clone -b ksun --depth 1 https://github.com/droidcore/android_kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635
 rm -rf kernel/xiaomi/sm8635-modules
 git clone -b lineage-23.0 --depth 1 https://github.com/droidcore/android_kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
 
 rm -rf kernel/xiaomi/sm8635-devicetrees
 git clone -b lineage-23.0 --depth 1 https://github.com/peridot-dev/android_kernel_xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
-=======
-git clone -b lineage-23.1 --depth 1 https://github.com/sm8635-dev/kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635
-
-rm -rf kernel/xiaomi/sm8635-modules
-git clone -b lineage-23.1 https://github.com/sm8635-dev/kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
-
-rm -rf kernel/xiaomi/sm8635-devicetrees
-git clone -b lineage-23.1 https://github.com/sm8635-dev/kernel_xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
-
-# MiuiCamera device tree (fresh clone)
-echo "Cloning MiuiCamera device tree..."
-rm -rf device/xiaomi/peridot-miuicamera
-git clone https://github.com/sm8635-dev/device_xiaomi_peridot-miuicamera.git device/xiaomi/peridot-miuicamera
-
-# MiuiCamera vendor tree (fresh clone)
-echo "Cloning MiuiCamera vendor tree..."
-rm -rf vendor/xiaomi/peridot-miuicamera
-git clone https://github.com/sm8635-dev/vendor_xiaomi_peridot-miuicamera.git vendor/xiaomi/peridot-miuicamera
->>>>>>> c4ab372 (peridot: Pin all firmware)
 
 # Hardware xiaomi (fresh clone)
 echo "Cloning hardware xiaomi source..."
 rm -rf hardware/xiaomi
-git clone -b lineage-23.1 https://github.com/sm8635-dev/hardware_xiaomi.git hardware/xiaomi
-<<<<<<< HEAD
+git clone -b lineage-23.0 https://github.com/lightbulb-box/hardware_xiaomi.git hardware/xiaomi
+
+# Dolby
+rm -rf packages/apps/XiaomiDolby
+git clone -b lineage-23.0 https://github.com/lightbulb-box/android_packages_apps_XiaomiDolby packages/apps/XiaomiDolby
 
 # MiuiCamera device tree (fresh clone)
 echo "Cloning MiuiCamera device tree..."
@@ -59,38 +38,16 @@ git clone https://github.com/F6-test/vendor-xiaomi-peridot-miuicamera.git vendor
 echo "Cloning Viper4Android tree..."
 rm -rf packages/apps/ViPER4AndroidFX
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
-=======
->>>>>>> c4ab372 (peridot: Pin all firmware)
 
 # KProfiles (fresh clone)
 echo "Cloning KProfiles..."
 rm -rf packages/apps/KProfiles
 git clone -b lineage-23.1 https://github.com/sm8635-dev/packages_apps_KProfiles.git packages/apps/KProfiles
 
-<<<<<<< HEAD
 # Gamebar
 echo "Cloning Gamebar tree..."
 rm -rf packages/apps/GameBar
 git clone https://github.com/droidcore/packages_apps_GameBar.git packages/apps/GameBar
-=======
-# FastCharge (fresh clone)
-echo "Cloning FastCharge..."
-rm -rf packages/apps/FastCharge
-git clone -b lineage-23.1 https://github.com/sm8635-dev/packages_apps_FastCharge.git packages/apps/FastCharge
->>>>>>> c4ab372 (peridot: Pin all firmware)
-
-# Compat (cherry pick)
-echo "Fetching QPR1 compat..."
-cd hardware/lineage/compat
-git fetch https://github.com/stx-staging/android_hardware_statix_compat bp3a
-git reset --hard FETCH_HEAD
-
-# Picking sepolicy for QPR1 (cherry pick)
-echo "Picking sepolicy fix..."
-cd ../../..
-cd device/qcom/sepolicy_vndr/sm8650
-git fetch https://github.com/sm8635-dev/device_qcom_sepolicy_vndr
-git cherry-pick 39cfd17977cc664fa8393b6569c39179f4127b2d 1d2c884133bb23d780fc35ecff27d2e6eeabe314 8e148a4417233704f40c223c0624d41f017b490e
 
 # Refresh signing keys
 if [ -d vendor/lineage-priv/keys ]; then
@@ -98,7 +55,7 @@ if [ -d vendor/lineage-priv/keys ]; then
   rm -rf vendor/lineage-priv/keys
 fi
 echo "Cloning fresh signing keys..."
-git clone https://github.com/droidcore/priv-key.git -b main vendor/lineage-priv/keys
+git clone https://github.com/AbuRider/priv_keys.git -b main vendor/lineage-priv/keys
 
 # Always back to root at the end
 if command -v croot &>/dev/null; then
